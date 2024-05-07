66dd6415b9fdcdc1dba8a54653498dfa0dbcf144
arun <arunks002info@gmail.com>
2024-05-07 23:10:33 +0530

    star rating

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+
+import './App.css'
+import StarRating from './components/star-rating'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+{/* <Accordian/> */}
+{/* <RandomColor/> */}
+<StarRating />
+
+       </div>
+    </>
+  )
+}
+
+export default App
